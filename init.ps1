f68beeecc07ed3361f1d38fec91ab14c634b555a
sblmnl <sblmnl@users.noreply.github.com>
2025-10-05 21:46:55 -0400

    set env vars in init scripts

--- init.ps1
+++ init.ps1
@@ -3,4 +3,9 @@ dotnet husky install
 
 cd client
 npm install
+
+Set-Content -Path ".env" -Value @"
+VITE_APP_PATH_BASE=""
+"@
+
 cd ..
--- init.sh
+++ init.sh
@@ -3,4 +3,9 @@ dotnet husky install
 
 cd client
 npm install
+
+cat <<EOF > .env
+VITE_APP_PATH_BASE=""
+EOF
+
 cd ..
